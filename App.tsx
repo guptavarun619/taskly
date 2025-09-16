72743a68829521a15aa42e5f8f3bdbcbb3066382
guptavarun619 <guptavarun619@gmail.com>
2025-09-17 00:21:04 +0530

    Added text box and button with native aleart

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from "react-native";
+import { theme } from "./theme";
 
-export default function App() {
+const App = () => {
+  const handleDelete = () => {
+    Alert.alert("Are you sure you want to delete this", "It'l be deleted for sure", [{
+      text: "Sure",
+      onPress: () => console.log('Deleted!'),
+      style: "destructive",
+    }, {
+      text: "Cancel",
+      style: "cancel",
+    }]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemText}> Coffee Latte</Text>
+        <TouchableOpacity onPress={handleDelete} activeOpacity={0.8} style={styles.button}>
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +29,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
   },
+  itemContainer: {
+    borderBottomWidth: 1,
+    margin: 16,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+    borderRadius: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  itemText: { fontSize: 18, fontWeight: '200'},
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6, 
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    letterSpacing: 1,
+  }
 });
+
+export default App;
